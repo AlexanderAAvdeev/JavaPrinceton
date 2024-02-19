518c36fc1c0d0103608cdc476757127a3af51928
AlexanderAAvdeev <gazon3306@gmail.com>
2024-02-19 22:27:10 +0300

    Commit for 09 week
    Description and homework for week 09

---
+++ 09_Creating Data Types/09_Assignment/Clock.java
@@ -0,0 +1,83 @@
+public class Clock {
+    private static final int HOURS = 24;
+    private static final int MINUTES = 60;
+    private int hour, minute;
+
+    // Creates a clock whose initial time is h hours and m minutes.
+    public Clock(int h, int m) {
+        validate(h, m);
+        hour = h;
+        minute = m;
+    }
+
+    // Creates a clock whose initial time is specified as a string, using the format HH:MM.
+    public Clock(String s) {
+        String regex = "^\\d{2}:\\d{2}";
+        if (!s.matches(regex))
+            throw new IllegalArgumentException("the time format must be HH:MM");
+        int h = Integer.parseInt(s.substring(0, 2));
+        int m = Integer.parseInt(s.substring(3));
+        validate(h, m);
+        hour = h;
+        minute = m;
+    }
+
+    // Throws an exception if the given time is invalid.
+    private static void validate(int h, int m) {
+        if (h < 0 || h >= HOURS)
+            throw new IllegalArgumentException("the hour must be between 0 and 23");
+        if (m < 0 || m >= MINUTES)
+            throw new IllegalArgumentException("the minutes must be between 0 and 59");
+    }
+
+    // Returns a string representation of this clock, using the format HH:MM
+    public String toString() {
+        String clock = "";
+        if (this.hour < 10) clock += "0";
+        clock = clock + this.hour + ":";
+        if (this.minute < 10) clock += "0";
+        clock += this.minute;
+        return clock;
+    }
+
+    // Is the time on this clock earlier than the time on that one?
+    public boolean isEalierThan(Clock that) {
+        if (this.hour > that.hour) return false;
+        if (this.hour == that.hour && this.minute >= that.minute) return false;
+        return true;
+    }
+
+    // Adds 1 minute to the time on this clock.
+    public void tic() {
+        toc(1);
+    }
+
+    // Adds ∆ minutes to the time on this clock.
+    public void toc(int delta) {
+        if (delta < 0)
+            throw new IllegalArgumentException("delta must be a positive integer");
+        int temp = this.minute + delta;
+        this.minute = temp % MINUTES;
+        this.hour = (this.hour + temp / MINUTES) % HOURS;
+    }
+
+    // test client
+    public static void main(String[] args) {
+        Clock clock1 = new Clock(23, 02);
+        Clock clock2 = new Clock("05:08");
+
+        StdOut.println("Clock 1: " + clock1.toString());
+        StdOut.println("Clock 2: " + clock2.toString());
+
+        StdOut.println("Clock 1 is ");
+        if (!clock1.isEalierThan(clock2))
+            StdOut.print("not ");
+        StdOut.println("earlier than Clock 2. ");
+
+        clock1.tic();
+        StdOut.println("Tic. Clock 1: " + clock1.toString());
+
+        clock2.toc(87);
+        StdOut.println("Toc. Clock 2: " + clock2.toString());
+    }
+}
